d79e6ca3c715a28ce496709073137b8101fc33b2
Ana Laura Cavagnaro <analauracavagnaro@gmail.com>
2022-08-27 18:42:48 -0300

    Creación de Administrador

--- AppNegocio/admin.py
+++ AppNegocio/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from django.contrib import admin
+from .models import*
+
+admin.site.register(Negocio)
+admin.site.register(Producto)
+admin.site.register(Empleado)
 
 # Register your models here.

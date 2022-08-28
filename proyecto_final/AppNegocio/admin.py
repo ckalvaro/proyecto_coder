65dca85a476318dc2b8e33f979304b9f37b739a8
Ana Laura Cavagnaro <analauracavagnaro@gmail.com>
2022-08-28 17:57:01 -0300

    borro archivos  de caché que van al gitignore

--- proyecto_final/AppNegocio/admin.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-from django.contrib import admin
-from .models import*
-
-admin.site.register(Negocio)
-admin.site.register(Producto)
-admin.site.register(Empleado)
-
-# Register your models here.
